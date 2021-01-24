11e011df19173ad8a8806d6d5a8358dbb65d2cfe
IoTDevelopmentTeam <anup.frndcircle31@gmail.com>
2021-01-24 21:02:12 +0530

    added data service

---
+++ iot-web/src/app/iot-data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IotDataService {
+
+  constructor() { }
+
+  //https://api.wisethingz.com/Dev/thingsapi?thingname=parv&count=3
+  
+}
+
